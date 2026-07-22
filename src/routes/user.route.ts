19e1a9209e4448482343f3419cb1e17d8989e6f7
rdsemon <codewithemons@gmail.com>
2026-07-22 22:48:33 +0600

    implement get and post route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { createUser, getAllUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/");
+router.post("/user", createUser);
+router.get("/user", getAllUser);
 
 export default router;
