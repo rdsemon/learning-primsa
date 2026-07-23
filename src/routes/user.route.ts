705685c274597ec51f0a3968c7de814c2ea982b4
rdsemon <codewithemons@gmail.com>
2026-07-23 08:34:04 +0600

    add new patch and delete route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { createUser, getAllUser } from "../controllers/user.controller.js";
+import {
+  createUser,
+  deleteUser,
+  getAllUser,
+  updateUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/user", createUser);
 router.get("/user", getAllUser);
+router.patch("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
 
 export default router;
