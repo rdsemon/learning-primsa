e22ec2422cbc06ef20eaf1187ccabd3c6c71ca15
rdsemon <codewithemons@gmail.com>
2026-07-23 08:32:51 +0600

    implement update and delete controller functions

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -12,6 +12,40 @@ export const createUser = async (req: Request, res: Response) => {
 
   const user = await prisma.user.create({ data: { name, email, posts } });
 
-  res.status(200).json({ status: "success", data: "user create success" });
+  res.status(201).json({ status: "success", data: "user create success" });
 };
 
+export const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const user = await prisma.user.findUnique({
+    where: { id: id as string },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const { name, posts } = req.body;
+
+  const updateUser = await prisma.user.update({
+    where: { id: id as string },
+    data: { name, posts },
+  });
+
+  res.status(200).json({ status: "success", message: "got the response" });
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { id: id as string },
+  });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  await prisma.user.delete({ where: { id: id as string } });
+
+  res.status(204).json({ status: "success", message: "Delete successfull" });
+};
